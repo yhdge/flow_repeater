e8d37ff915a119c63eb2330b9c61e6eef6bcab0f
liuyuanhang <liuyuanhang@antiy.cn>
2020-05-28 15:29:27 +0800

    流量转发器ipv4

--- socket_class.py
+++ socket_class.py
@@ -1,34 +1,99 @@
+# -*- encoding: utf-8 -*-
 '''
-@Author: Voyage
-@Date: 2020-05-11 20:59:33
-@LastEditTime: 2020-05-11 21:22:08
-@Description: flow repeater main class
-@FilePath: \flow_repeater\socket_class.py
+@Time    :   2020/05/28
+@Author  :   Voyage/YH21219
 '''
-import gevent
-from gevent import socket, monkey
-monkey.patch_all()
+from IPy import IP
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+from threading import Thread
 
 
-class FlowRepeater(object):
+class NetForward(object):
 
-    def __init__(self, local_listen_addr, local_listen_port, target_ip, target_port):
-        self.local_listen_addr = local_listen_addr
-        self.local_listen_port = local_listen_port
-        self.target_ip = target_ip
-        self.target_port = target_port
-    
-    def listen(self):
-        local_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __init__(self, listen_port, remote_ipaddr, remote_port, listen_addr="0.0.0.0"):
+        #  listen_addr 本机监听地址 0.0.0.0 代表接收所有的流量
+        #  listen_port 本机监听端口
+        #  remote_ipaddr  远程ip地址
+        #  remote_port 远程端口
+        self.listen_addr = listen_addr
+        self.listen_port = listen_port
+        self.remote_ipaddr = remote_ipaddr
+        self.remote_port = remote_port
+
+    def init_check(self):
         try:
-            local_server.bind((self.local_listen_addr, self.local_listen_port))
+            IP(self.listen_addr)._ipversion
+            IP(self.remote_ipaddr)._ipversion
+        except:
+            exit()
+        if not isinstance(self.listen_port, int) or not isinstance(self.remote_port, int):
+            print("端口必须为整数")
+            exit()
+        assert self.listen_port > 1 and self.listen_port <= 65535, "端口数必须在2-65535之间"
+        assert self.remote_port > 1 and self.remote_port <= 65535, "端口数必须在2-65535之间"
+
+    def socket_server(self):        # 本机监听socket
+        local_server = socket(AF_INET, SOCK_STREAM)
+        local_server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        try:
+            local_server.bind((self.listen_addr, self.listen_port))
         except Exception as e:
             print(e)
-        local_server.listen(20)
+            return
+        return local_server
+
+    def socket_client(self):    # 本机客户端连接远程socket
+        remote_conn = socket(AF_INET, SOCK_STREAM)
+        try:
+            remote_conn.connect((self.remote_ipaddr, self.remote_port))
+        except Exception as e:
+            print(e)
+            return
+        return remote_conn
+
+    def deal_netflow(self, local_conn, remote_conn):
+        if not local_conn or not remote_conn:
+            return
+        while True:
+            try:
+                data = local_conn.recv(4096)
+            except Exception as e:
+                print(e)
+                break
+            if not data:
+                break
+            try:
+                remote_conn.sendall(data)
+            except Exception as e:
+                print(e)
+                break
+        local_conn.close()
+        remote_conn.close()
+
+    def tcp_request_deal(self, local_conn):     # 流量请求处理
+        remote_conn = self.socket_client()
+        if not remote_conn:
+            return
+        Thread(target=self.deal_netflow, args=(
+            local_conn, remote_conn)).start()
+        Thread(target=self.deal_netflow, args=(
+            remote_conn, local_conn)).start()
+
+    def listen(self):
+        self.init_check()
+        local_server = self.socket_server()
+        if not local_server:
+            return
+        local_server.listen(30)
+        print("local listen port:{0} remoteip:{1} remoteport:{2}".format(
+            self.listen_port, self.remote_ipaddr, self.remote_port))
         while True:
             local_conn, local_addr = local_server.accept()
-            gevent.spawn(self.tcp_forward_request, local_conn)
-    
-    def forward_request(self, local_conn):
-        remote_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        remote_conn.connect((self.target_ip, self.target_port))
+            Thread(target=self.tcp_request_deal, args=(local_conn,)).start()
+
+
+if __name__ == "__main__":
+    s = NetForward(12345, "************", 23456, "0.0.0.0")
+    # 本机监听端口，远程ip地址，远程接收端口，本机监听地址
+    s.listen()
+
